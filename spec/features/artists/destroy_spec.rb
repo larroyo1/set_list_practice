98fa1a596469b37bc5cf80d5127dd5b1b23c859b
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-09-07 00:39:10 -0600

    added ability to delete artist

---
+++ spec/features/artists/destroy_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'deleting artist' do
+
+  it 'can delete the artist from the index page' do
+    artist = Artist.create!(name: 'Prince')
+    visit '/artists'
+    click_button 'Delete'
+    expect(current_path).to eq('/artists')
+    expect(page).to_not have_content('Prince')
+  end
+end
